afd70a50834b838900259565ffd1991d059d11f8
Hüseyn Alıyev <hajiaghaaa@code.edu.az>
2024-08-11 23:59:20 +0400

    AddedViewsToProject

---
+++ Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Karma.MVC.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
